8fbcc6b4766ed39625bf4d46ec129faa423dd26b
dsnam <dodsonn@gmail.com>
2018-06-05 01:08:05 -0400

    Added support for several new endpoints and parameters. Refactored existing functions.

--- README.md
+++ README.md
@@ -1,23 +1,26 @@
 # nhl-api
-Most of the get_ functions just return the json response, other functions were written specifically to
-process the data for another project, so they may not be too useful to anyone else.
+The get_ functions just return the json response, the other functions return processed data as lists or dicts.
 
 Currently supported:
 
-json results for teams, schedule, standings
+json results for teams, schedule, standings, players, teams, stats, player stats.
 
-You can also get a list of teams and their api ids 
+You can also get a list of namedtuples of teams and their api ids
 ```python
-(18, 'Nashville Predators')
+('18', 'Nashville Predators')
 ```
-This tuple format is used to index into the other processed data.
+These namedtuples are used to index into the other processed data. Players are handled the same way.
 
 points/games played and conference/division info for each team: 
 ```python
-{(18, 'Nashville Predators): {'conf': 'Western', 'div': 'Central', 'pts': 72, 'gp': 52}}
+{('18', 'Nashville Predators): {'conference': 'Western', 'division': 'Central', 'points': 72, 'games_played': 52}}
 ```
 
 as well as each team's remaining games against every other team:
 ```python
-{(id, 'Team Name'): {(id, 'Team Name'): games_remaining ... } }
+{('id', 'Team Name'): {('id', 'Team Name'): games_remaining ... } }
 ```
+
+Still to come:
+Full support for all endpoint parameters
+Support for game, prospects, awards, draft endpoints
--- nhl_api.py
+++ nhl_api.py
@@ -1,54 +1,75 @@
 """
 Wrapper for the NHL API, plus some functions that strip out specific info that I've needed
+All get_ functions return json responses. Other functions return processed results as lists, dicts, etc.
+Format for all years is YYYY-MM-DD
 """
 
 import requests
-import datetime
+from datetime import date
+from collections import namedtuple
 
 
 class NHL:
 
     def __init__(self):
-        self.nhl_ep = 'https://statsapi.web.nhl.com/api/v1/'
+        self.nhl_endpoint = 'https://statsapi.web.nhl.com/api/v1/'
+        self.Team = namedtuple('Team', ['team_id', 'name'])
+        self.Player = namedtuple('Player', ['player_id', 'name'])
+        self.teams = []
+        self.players = []
 
-    def get(self, endpoint, params=[]):
+    def get(self, endpoint, params={}):
         """
         :param endpoint: nhl api endpoint
-        :param params: parameters such as startDate, etc. will list these at some point
+        :param params: parameters for the endpoint. see specific get_ methods for detail
         :return: json of the response
         """
-        ep = self.nhl_ep + endpoint
+        endpoint = self.nhl_endpoint + endpoint
         if params:
-            data = requests.get(ep, params).json()
-        else:
-            data = requests.get(ep).json()
+            return requests.get(endpoint, params).json()
 
-        return data
+        return requests.get(endpoint).json()
 
-    def get_schedule(self, start_date, end_date):
+    def get_schedule(self, start_date=date.today().strftime("%Y-%m-%d"), end_date=date.today().strftime("%Y-%m-%d"),
+                     linescores=False, broadcasts=False, ticket_info=False, team_id=None):
         """
         :param start_date: start date for the schedule pull
         :param end_date: end date for the schedule
+        :param linescores: include linescore for completed games
+        :param broadcasts: show broadcasts of the game
+        :param ticket_info: show ticket purchasing info
+        :param team_id: limit the schedule result to a team or list of teams
         :return: json of the schedule data
         """
-
-        ep = 'schedule'
+        endpoint = 'schedule'
         params = {'startDate': start_date, 'endDate': end_date}
-        return self.get(ep, params)
+        expand_params = []
+        if linescores:
+            expand_params.append('schedule.linescore')
+        if ticket_info:
+            expand_params.append('schedule.ticket')
+        if broadcasts:
+            expand_params.append('schedule.broadcasts')
+        if expand_params:
+            params['expand'] = expand_params
+        if team_id:
+            params['id'] = team_id
 
-    def get_remaining_games_against(self):
+        return self.get(endpoint, params)
+
+    def remaining_games_against(self, start_date=date.today(), team_id=None):
         """
+        :param start_date: start date for calculating games remaining. attempts to be smart: if the provided date
+               is after when the regular season usually ends, then it will try to find schedule info for next year
+        :param team_id: limit result to a team or list of teams
         :return: remaining regular season games for each team, split by opponent
         """
-
         teams = self.get_teams_list()
         remaining_by_team = {team: {t: 0 for t in teams if t != team} for team in teams}
-        today = datetime.datetime.now()
-        # should be safe to assume the season ends sometime in April
-        schedule = self.get_schedule(today.strftime("%Y-%m-%d"), today.strftime("%Y")+'-04-30')
-        for date in schedule['dates']:
-            for game in date['games']:
-                # only want unfinished regular season games
+
+        schedule = self.get_schedule(start_date, start_date.strftime("%Y")+'-04-30')
+        for game_dates in schedule['dates']:
+            for game in game_dates['games']:
                 if game['gameType'] == 'R' and game['status']['abstractGameState'] != 'Final':
                     home = (game['teams']['home']['team']['id'], game['teams']['home']['team']['name'])
                     away = (game['teams']['away']['team']['id'], game['teams']['away']['team']['name'])
@@ -61,37 +82,99 @@ class NHL:
         """
         :return: json of the teams list
         """
-        ep = 'teams'
-        return self.get(ep)
+        endpoint = 'teams'
 
-    def get_teams_list(self):
+        return self.get(endpoint)
+
+    def teams_list(self, refresh=False):
         """
-        :return: processes the response to return a list of (id, name) tuples
+        :param refresh: calls the api again instead of returning cached data
+        :return: processes the response to return a list of Team namedtuples
         """
-        data = self.get_teams()
-        teams = []
-        for team in data['teams']:
-            teams.append((team['id'], team['name']))
+        if refresh or not self.teams:
+            for team in self.get_teams()['teams']:
+                self.teams.append(self.Team(str(team['id']), team['name']))
 
-        return teams
+        return self.teams
 
-    def get_standings(self):
-        ep = 'standings'
+    def get_standings(self, season=None, as_of_date=None, detail=False):
+        """
+        :param season: needs to be a string of format YYYYYYYY such as 20172018
+        :param as_of_date: standings up to a certain date YYYY-MM-DD
+        :param detail: include detailed information like home and away records
+        :return:
+        """
+        endpoint = 'standings'
+        params = {}
+        if season:
+            params['season'] = season
+        if date:
+            params['date'] = as_of_date
+        if detail:
+            params['expand'] = 'standings.record'
 
-        return self.get(ep)
+        return self.get(endpoint, params)
 
-    def get_points_gp(self):
+    def points_games_played(self):
         standings = self.get_standings()
         table = {}
         for record in standings['records']:
             div = (record['division']['id'], record['division']['name'])
             conf = (record['conference']['id'], record['conference']['name'])
             for team_record in record['teamRecords']:
-                team = (team_record['team']['id'], team_record['team']['name'])
-                table[team] = {'conf': conf, 'div': div, 'pts': team_record['points'], 'gp': team_record['gamesPlayed']}
+                team = self.Team(team_record['team']['id'], team_record['team']['name'])
+                table[team] = {'conference': conf, 'division': div, 'points': team_record['points'],
+                               'games_played': team_record['gamesPlayed']}
 
         return table
 
+    def get_roster(self, team_id: str):
+        endpoint = ''.join(['teams/', team_id, '/roster'])
 
+        return self.get(endpoint)
 
+    def team_roster(self, team_id: str):
+        """
 
+        :param team_id: team for which to look up the roster
+        :return: A list of Player named tuples
+        """
+        roster = []
+        for player in self.get_roster(team_id)['roster']:
+            roster.append(self.Player(player['person']['id'], player['person']['fullName']))
+
+        return roster
+
+    def players(self, refresh=False):
+        """
+        :param refresh: calls the api again instead of returning cached data
+        :return: a list of Player namedtuples
+        """
+        if refresh or not self.players:
+            for team in self.get_teams_list():
+                roster = self.get_roster(team.team_id)['roster']
+                for player in roster:
+                    self.players.append(self.Player(str(player['person']['id']), player['person']['fullName']))
+
+        return self.players
+
+    def get_player(self, player_id: str):
+        endpoint = ''.join(['people/', player_id])
+
+        return self.get(endpoint)
+
+    def get_player_stats(self, player_id: str, goals_by_situation=False):
+        """
+        :param player_id: id of player to fetch stats for
+        :param goals_by_situation: include goals by situation data
+        :return: json player stats response
+        """
+        endpoint = ''.join(['people/', player_id, '/stats'])
+        params = {}
+        stats = []
+        if goals_by_situation:
+            stats.append('goalsByGameSituation')
+        if stats:
+            params['stats'] = stats
+
+        return self.get(endpoint, params)
